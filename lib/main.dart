e859e056beb4c748de2cf54fddaeec94b2a51f2b
The Verify <theverifyofficial@gmail.com>
2026-05-25 17:49:35 +0530

    25/may/2026 by Lokesh mac

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,6 @@ import 'package:provider/provider.dart';
 import 'Notifier_provider/SignUp.dart';
 import 'views/auth/splash_screen.dart';
 
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
@@ -15,7 +14,6 @@ void main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
